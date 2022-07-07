55df4b0a76952649c8ab93988a1dfb3f0449fc0b
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2022-07-07 17:21:29 -0400

    initial config login

--- projects/ngx-network-error-handler/src/handler-http/Components/login/login.module.ts
+++ projects/ngx-network-error-handler/src/handler-http/Components/login/login.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+// Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [LoginComponent]
 })
